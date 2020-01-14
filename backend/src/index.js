3b1aad2ee7b0dc6966821ce6f5d092a1a7cc5782
Douglas Varollo <dcvarollo@gmail.com>
2020-01-14 19:21:27 -0300

    Configurações do mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const routes = require("./routes");
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(express.json());
 app.use(routes);
