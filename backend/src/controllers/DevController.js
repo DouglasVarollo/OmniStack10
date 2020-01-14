90218ca3b0301a9ff15b03dba056f0eac8c0e0a7
Douglas Varollo <dcvarollo@gmail.com>
2020-01-14 19:37:16 -0300

    Lista todos os Devs a partir de uma latitude e longitude

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 
 const Dev = require("../models/Dev");
+const parseStringAsArray = require("../utils/parseStringAsArray");
 
 module.exports = {
   async index(request, response) {
@@ -21,7 +22,7 @@ module.exports = {
 
       const { name = login, avatar_url, bio } = apiResponse.data;
 
-      const techsArray = techs.split(",").map(tech => tech.trim());
+      const techsArray = parseStringAsArray(techs);
 
       const location = {
         type: "Point",
