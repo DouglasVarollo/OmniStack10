d24568936a2f60ac72c1901609854e0fdbea3dea
Douglas Varollo <dcvarollo@gmail.com>
2020-01-14 19:33:39 -0300

    Lista todos os Devs

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -3,6 +3,12 @@ const axios = require("axios");
 const Dev = require("../models/Dev");
 
 module.exports = {
+  async index(request, response) {
+    const devs = await Dev.find();
+
+    return response.json(devs);
+  },
+
   async store(request, response) {
     const { github_username, techs, latitude, longitude } = request.body;
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const DevController = require("./controllers/DevController");
 
 const routes = Router();
 
+routes.get("/devs", DevController.index);
 routes.post("/devs", DevController.store);
 
 module.exports = routes;
