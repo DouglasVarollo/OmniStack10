451f4e0f44c3dce385a2c512e72a3e4042dd13a7
Douglas Varollo <dcvarollo@gmail.com>
2020-01-15 21:03:06 -0300

    Estilos globais e do formúlario

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/app/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function App() {
-  return <h1>Hello OmniStack</h1>;
-}
-
-export default App;
