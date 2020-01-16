b5d7aa6ccddffe40b542adbeab88a8858a1e0f98
Douglas Varollo <dcvarollo@gmail.com>
2020-01-15 21:30:01 -0300

    Adiciona o cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const routes = require("./routes");
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
