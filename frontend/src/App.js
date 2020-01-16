6e2795687b75186ea123b9608779fb3c0798ced4
Douglas Varollo <dcvarollo@gmail.com>
2020-01-15 21:16:31 -0300

    Estlização dos usuários

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
+import "./Main.css";
 
 function App() {
   return (
@@ -37,7 +38,89 @@ function App() {
         </form>
       </aside>
 
-      <main></main>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
+                alt="Diego Fernandes"
+              />
+
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+
+            <p>
+              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
+              desenvolvimento web e mobile.
+            </p>
+            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
+                alt="Diego Fernandes"
+              />
+
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+
+            <p>
+              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
+              desenvolvimento web e mobile.
+            </p>
+            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
+                alt="Diego Fernandes"
+              />
+
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+
+            <p>
+              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
+              desenvolvimento web e mobile.
+            </p>
+            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
+                alt="Diego Fernandes"
+              />
+
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+
+            <p>
+              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
+              desenvolvimento web e mobile.
+            </p>
+            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
