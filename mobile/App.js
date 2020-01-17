8595cf78f71f4dca8a10bdbede1b8f14e5c1633e
Douglas Varollo <dcvarollo@gmail.com>
2020-01-17 15:06:27 -0300

    Estrutura configurada

---
+++ mobile/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello OmniStack</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#7159c1"
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#fff"
+  }
+});
